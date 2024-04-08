c562c76804afb91b1dbc000090e1c91503f41fd6
ilayda <48770441+elflyd@users.noreply.github.com>
2024-04-08 14:35:34 +0300

    Update ElementHelper.java

--- src/main/java/Util/ElementHelper.java
+++ src/main/java/Util/ElementHelper.java
@@ -21,9 +21,7 @@ public class ElementHelper {
     public WebElement presenceElement(By key) {
         return
                 wait.until(ExpectedConditions.presenceOfElementLocated(key));
-
     }
-
     public WebElement findElement(By key) {
         WebElement element = presenceElement(key);
         return element;
@@ -35,5 +33,8 @@ public class ElementHelper {
 
     public void sendKeys(By key, String text){
         findElement(key).sendKeys(text);
+    }
+    public void checkVisible(By key){
+        wait.until(ExpectedConditions.visibilityOf(findElement(key)));
     }
 }
